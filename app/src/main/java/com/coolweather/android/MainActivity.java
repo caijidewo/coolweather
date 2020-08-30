fc57ca19116ebf636e2efeaabe119952c6e40855
zhidong li <2859208791@qq.com>
2020-08-30 23:59:40 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -8,6 +8,7 @@ import android.preference.PreferenceManager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.coolweather.android.gson.Weather;
+import com.coolweather.android.service.AutoUpdateService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         if (prefs.getString("weather", null)!= null) {
+            Intent service = new Intent(this, AutoUpdateService.class);
+            startService(service);
             Intent intent = new Intent(this, WeatherActivity.class);
             startActivity(intent);
             finish();
